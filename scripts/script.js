871700040a33dbf0094d32914df74feb8ecc574e
Ricardo <112439514+thericardoac@users.noreply.github.com>
2023-01-14 11:26:53 -0600

    Change the way to use the drawing board. - Since the original way to use the canvas (just hovering to paint) causes the user to start painting as soon as it enters the canvas, it was considered to implement a click-and-drag feature to paint inside the canvas, but it introduced a bug where if the user, not knowing, clicks-and-drags the invisible dot border, it will continue to paint/erase even if the user is no longer holding the mouse click.
    Because of both methods having disadvantages, it is that now, the canvas
    will be used with a click-to-toggle function, requiring the user to click
    inside the canvas to start using it and click again to stop. User can
    double click to paint/erase a single dot.
    
    Changes list:
    - One click inside the canvas enables/disables the use of the dot (Toggles
    useDot true or false).
    - Double click paints/erases a single dot in the canvas.
    - The mouse-over event, will paint/erase the dot if useDot variable is true.
    - If user "leaves" canvas, the useDot variable turns false.
    - Edited some comments.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // ****************************** VARIABLE DECLARATIONS *****************************
-// Variables for canvas, controls and modes
+// Variables for canvas, controls, modes and use of dot.
 const canvas = document.querySelector("#canvas");
 const slctSize = document.querySelector("#slct-canvas-size");
 let selectedSize = slctSize.value;
@@ -17,40 +17,62 @@ let useDot = false;
 
 
 // ******************************* FUNCTION DECLARATIONS ****************************
-// Creates the dots inside the canvas
+// Creates the dots inside the canvas.
 function createCanvasDots(selectedSize) {
-    // Sets canvas columns accordingly to selected size
+    // Sets canvas columns accordingly to selected size.
     canvas.style.gridTemplateColumns = "repeat(" + selectedSize + ", 1fr";        
 
-    // Multiply height by width to set the total dots to create
+    // Multiply height by width to set the total dots to create.
     let dotsToDraw = selectedSize * selectedSize;    
     let canvasDot;
     
+    // Creates the dots inside the canvas.
     for (let i = 1; i <= dotsToDraw; i++) {
         canvasDot = document.createElement("div");
         canvasDot.className = "canvas-dot";
         canvasDot.id = "dot-" + i;                 
         canvas.appendChild(canvasDot);
         
-        //Adds painting or erasing functionality to recently created dot        
+        // If user hovers the dot and useDot is true, will color or erase the dot.      
         canvasDot.addEventListener("mouseover", function() {                              
+            if (useDot) {
+                if (!eraserMode) {
+                    colorDot(i);
+                } else {
+                    eraseDot(i);
+                } 
+            }                       
+        });
+        
+        // Paints or erases a single dot in canvas at double click.
+        canvasDot.addEventListener("dblclick", function() {            
             if (!eraserMode) {
                 colorDot(i);
             } else {
                 eraseDot(i);
-            }            
-        });        
+            }                          
+        });
+
+        // Toggles the use of the dot (interaction with the dot).
+        canvasDot.addEventListener("click", function() {
+            if (!useDot) {
+                useDot = true;
+        
+            } else {
+                useDot = false;
+            }          
+        });
     }
 
-    // Disables eraser mode so the user, can start drawing at canvas creation
-    //Useful if eraser mode is enabled at the time of clearing or selecting a new size canvas 
+    // Disables eraser mode so the user, can start drawing at canvas creation.
+    // Useful if eraser mode is enabled at the time of clearing or selecting a new size canvas.
     if (eraserMode) {
         toggleEraserMode();
     }
 }
 
 
-// Deletes every dot inside the canvas
+// Deletes (removes from DOM) every dot inside the canvas.
 function deleteCanvasDots() {
     let dotCount = canvas.querySelectorAll(".canvas-dot").length;
     let dotToDelete;    
@@ -62,7 +84,7 @@ function deleteCanvasDots() {
 }
 
 
-// Toggles Rainbow mode and applies style to rainbow button
+// Toggles Rainbow mode and applies style to rainbow button.
 function toggleRainbowMode() {
     if (!rainbowMode) {
         rainbowMode = true;        
@@ -70,11 +92,11 @@ function toggleRainbowMode() {
         rainbowMode = false;
         dotColor = colorPicker.value;
     }
-    btnRainbow.classList.toggle("rainbow-on");    
+    btnRainbow.classList.toggle("rainbow-on");  
 }
 
 
-// Toggles Eraser mode and applies style to eraser button
+// Toggles Eraser mode and applies style to eraser button.
 function toggleEraserMode() {
     if (!eraserMode) {
         eraserMode = true;
@@ -83,7 +105,7 @@ function toggleEraserMode() {
         eraserMode = false;
         dotColor = colorPicker.value;
     }
-    btnEraser.classList.toggle("eraser-on");    
+    btnEraser.classList.toggle("eraser-on");   
 }
 
 
@@ -97,7 +119,7 @@ function generateRandomColor() {
 }
 
 
-// Colors the selected dot
+// Colors the selected dot.
 function colorDot(i) {
     let dotToPaint = document.querySelector("#dot-" + i);
 
@@ -110,34 +132,27 @@ function colorDot(i) {
 }
 
 
-// Erases the selected dot
+// Erases the selected dot. (Clears the color).
 function eraseDot(i) {
     let dotToErase = document.querySelector("#dot-" + i);
     dotToErase.style.background = "none";    
 }
-// *******************************************************************************
 
 
-
-// **************************** RUNTIME START ************************************
-// Variables for canvas, controls and modes
-// const canvas = document.querySelector("#canvas");
-// const slctSize = document.querySelector("#slct-canvas-size");
-// let selectedSize = slctSize.value;
-// const colorPicker = document.querySelector("#color-picker");
-// let dotColor = colorPicker.value;
-// const btnRainbow = document.querySelector("#btn-rainbow-mode");
-// let rainbowMode = false;
-// const btnEraser = document.querySelector("#btn-eraser");
-// let eraserMode = false;
-// const btnClearCanvas = document.querySelector("#btn-clear-canvas");
-
-// Creates dots inside the canvas accordingly with the default size selected 
+// ***************************** POPULATES THE CANVAS AT PAGE LOAD **************************************
+// Creates dots inside the canvas accordingly with the default size selected. 
 createCanvasDots(selectedSize);
 
-// ***************************** GUI - EVENT LISTENERS **************************************
+// If user leaves the canvas, the use of dot (interaction) gets disabled.
+canvas.addEventListener("mouseleave", function() {
+    useDot = false;    
+});
+
+
+
+// ***************************** GUI ELEMENTS - EVENT LISTENERS **************************************
 // SIZE SELECTOR
-// When size changes, deletes and creates the canvas dots again
+// When size changes, deletes and creates the canvas dots again.
 slctSize.addEventListener("change", function() {
     selectedSize = slctSize.value;
     deleteCanvasDots();
@@ -146,7 +161,7 @@ slctSize.addEventListener("change", function() {
 
 
 // COLOR PICKER
-// Gets the color selected by the user. Disables Rainbow or Eraser modes if they are enabled
+// Gets the color selected by the user. Disables Rainbow or Eraser modes if they are enabled.
 colorPicker.addEventListener("change", function() {
     dotColor = colorPicker.value;
     if (eraserMode) {
@@ -159,7 +174,7 @@ colorPicker.addEventListener("change", function() {
 
 
 //RAINBOW MODE BUTTON
-// Toggles Rainbow mode and disables Eraser mode if it is enabled
+// Toggles Rainbow mode and disables Eraser mode if it is enabled.
 btnRainbow.addEventListener("click", function() {
     toggleRainbowMode();    
     if (eraserMode) {
@@ -169,7 +184,7 @@ btnRainbow.addEventListener("click", function() {
 
 
 //ERASER MODE BUTTON
-// Toggles Eraser mode and disables Rainbow mode if it is enabled
+// Toggles Eraser mode and disables Rainbow mode if it is enabled.
 btnEraser.addEventListener("click", function() {
     toggleEraserMode();
     if (rainbowMode) {
@@ -179,7 +194,7 @@ btnEraser.addEventListener("click", function() {
 
 
 // CLEAR CANVAS BUTTON
-// Deletes and creates the canvas dots again
+// Deletes and creates the canvas dots again.
 btnClearCanvas.addEventListener("click", function() {
     deleteCanvasDots();
     createCanvasDots(selectedSize);
